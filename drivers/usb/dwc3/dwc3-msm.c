6d60f7342500fff13b7ded7fdb60ac41f2e46c9c
Azhar Shaikh <azhars@codeaurora.org>
2014-11-11 18:59:20 -0800

    usb: dwc3-msm: Mask events on cable disconnect
    If erratic event occurs upon cable disconnect, then controller is
    generating erratic events even after block reset is done.
    Hence prevent this situaton by masking all events on cable disconnect
    and after handling first block reset.
    Blocking the subsequent events would later prevent
    the unclocked register access due to controller suspend turning
    off core clock while block reset work is in progress.
    
    CRs-fixed: 754183
    Change-Id: Iab364f0f72bc467067471835101a4931f786e61e
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>
    
    Conflicts:
    	drivers/usb/dwc3/dwc3-msm.c

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1077,9 +1077,11 @@ static void dwc3_block_reset_usb_work(struct work_struct *w)
 	unsigned long flags;
 
 	dev_dbg(mdwc->dev, "%s\n", __func__);
-
 	dwc3_msm_block_reset(&mdwc->ext_xceiv, true);
-	dwc3_gadget_enable_irq(dwc);
+	if (mdwc->ext_xceiv.bsv) {
+		dbg_event(0xFF, "BR EnEVT", 0);
+		dwc3_gadget_enable_irq(dwc);
+	}
 	spin_lock_irqsave(&dwc->lock, flags);
 	dwc->err_evt_seen = 0;
 	spin_unlock_irqrestore(&dwc->lock, flags);
@@ -1585,6 +1587,43 @@ static void dwc3_wait_for_ext_chg_done(struct dwc3_msm *mdwc)
 
 }
 
+static void dwc3_flush_event_buffers(struct dwc3_msm *mdwc)
+{
+	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
+	int i;
+	unsigned long flags;
+
+	/* Only disable/flush all events when cable is disconnected */
+	if (mdwc->ext_xceiv.bsv)
+		return;
+
+	/* Disable all events on cable disconnect */
+	dbg_event(0xFF, "Dis EVT", 0);
+	dwc3_gadget_disable_irq(dwc);
+
+	/* Skip remaining events on disconnect */
+	spin_lock_irqsave(&dwc->lock, flags);
+	for (i = 0; i < dwc->num_event_buffers; i++) {
+		struct dwc3_event_buffer *evt;
+		evt = dwc->ev_buffs[i];
+		evt->lpos = (evt->lpos + evt->count) %
+			DWC3_EVENT_BUFFERS_SIZE;
+		evt->count = 0;
+		evt->flags &= ~DWC3_EVENT_PENDING;
+	}
+	spin_unlock_irqrestore(&dwc->lock, flags);
+
+	/*
+	 * If there is a pending block reset due to erratic event,
+	 * wait for it to complete
+	 */
+	if (dwc->err_evt_seen) {
+		dbg_event(0xFF, "Flush BR", 0);
+		flush_work(&mdwc->usb_block_reset_work);
+		dwc->err_evt_seen = 0;
+	}
+}
+
 static void dwc3_resume_work(struct work_struct *w)
 {
 	struct dwc3_msm *mdwc = container_of(w, struct dwc3_msm,
@@ -1596,6 +1635,8 @@ static void dwc3_resume_work(struct work_struct *w)
 		dev_dbg(mdwc->dev, "%s: notifying xceiv event\n", __func__);
 		if (mdwc->otg_xceiv) {
 			dwc3_wait_for_ext_chg_done(mdwc);
+			/* Handle erratic events during cable disconnect */
+			dwc3_flush_event_buffers(mdwc);
 			mdwc->ext_xceiv.notify_ext_events(mdwc->otg_xceiv->otg,
 							DWC3_EVENT_XCEIV_STATE);
 		}
@@ -1608,6 +1649,11 @@ static void dwc3_resume_work(struct work_struct *w)
 	} else {
 		pm_runtime_get_sync(mdwc->dev);
 		if (mdwc->otg_xceiv) {
+			/*
+			 * Handle erratic events during bus suspend and cable
+			 * disconnect
+			 */
+			dwc3_flush_event_buffers(mdwc);
 			mdwc->ext_xceiv.notify_ext_events(mdwc->otg_xceiv->otg,
 							DWC3_EVENT_PHY_RESUME);
 		} else if (mdwc->scope == POWER_SUPPLY_SCOPE_SYSTEM) {
@@ -3008,6 +3054,11 @@ static int dwc3_msm_pm_resume(struct device *dev)
 
 		/* Let OTG know about resume event and update pm_count */
 		if (mdwc->otg_xceiv) {
+			/*
+			 * Handle erratic events on bus suspend, PM suspend
+			 * and cable disconnect
+			 */
+			dwc3_flush_event_buffers(mdwc);
 			mdwc->ext_xceiv.notify_ext_events(mdwc->otg_xceiv->otg,
 							DWC3_EVENT_PHY_RESUME);
 			if (mdwc->ext_xceiv.otg_capability)
