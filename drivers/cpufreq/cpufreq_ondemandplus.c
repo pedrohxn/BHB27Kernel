acb73c34cc79d51506757f2e43b08d4424cb4732
bhb27 <fella.motog@gmail.com>
2015-10-04 14:07:39 -0300

    cpufreq_ondemandplus: fix min frequency
    This fix min freq from 300 to 268

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -96,7 +96,7 @@ static unsigned long up_threshold;
 #define DEFAULT_DOWN_DIFFERENTIAL 20
 static unsigned long down_differential;
 
-#define DEFAULT_MIN_FREQ 300000
+#define DEFAULT_MIN_FREQ 268800
 static u64 allowed_min;
 
 #define DEFAULT_MAX_FREQ 2265600
@@ -105,10 +105,10 @@ static u64 allowed_max;
 #define DEFAULT_INTER_HIFREQ 1728000
 static u64 inter_hifreq;
 
-#define DEFAULT_INTER_LOFREQ 300000
+#define DEFAULT_INTER_LOFREQ 268800
 static u64 inter_lofreq;
 
-#define SUSPEND_FREQ 300000
+#define SUSPEND_FREQ 268800
 static u64 suspend_frequency;
 
 #define DEFAULT_INTER_STAYCYCLES 2
