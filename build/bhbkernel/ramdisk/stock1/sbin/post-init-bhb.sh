e9d23c5b813172bfcbccee005534b4ba89385ed7
Felipe de Leon <fglfgl27@gmail.com>
2016-07-17 17:58:43 -0300

    Sbin: Update busybox to 1.24.2 Stericson + update sh to use new bb
    Signed-off-by: Felipe de Leon <fglfgl27@gmail.com>

--- build/bhbkernel/ramdisk/stock1/sbin/post-init-bhb.sh
+++ build/bhbkernel/ramdisk/stock1/sbin/post-init-bhb.sh
@@ -1,22 +1,21 @@
-#!/sbin/busybox sh
+#!/system/bin/sh
 #script init pull from neobuddy89 github
 
 # Mount root as RW to apply tweaks and settings
-mount -o remount,rw /;
+mount -o remount,rw /
 mount -o rw,remount /system
 
+# Give permissions to execute
+chmod -R 777 /tmp/
+chmod 6755 /sbin/*
+chmod 6755 /system/xbin/*
+
 # Make tmp folder
-if [ -e /tmp]; then
+if [ -e /tmp ]; then
 	echo "tmp already exist"
 else
-mkdir /tmp;
+mkdir /tmp
 fi
-
-# Give permissions to execute
-chmod -R 777 /tmp/;
-chmod 6755 /sbin/*;
-chmod 6755 /system/xbin/*;
-echo "BHB27-Kernel Boot initiated on $(date)" > /tmp/bootcheck-bhb;
 
 #enable, disable and tweak some features of the kernel by default for better performance vs battery
 
@@ -44,5 +43,7 @@ chmod 444 /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
 echo 600000000 > /sys/devices/fdb00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/max_gpuclk
 echo Y > /sys/module/adreno_idler/parameters/adreno_idler_active
 
-exit;
 
+echo "Post init Kernel Boot initiated on $(date)" >> /tmp/bootcheck.txt
+
+exit
