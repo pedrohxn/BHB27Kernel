204a5b7350aef2341f5e1c3caef026486f5dbe28
bhb27 <fglfgl27@gmail.com>
2016-07-31 17:33:07 -0300

    Revert "cpufreq: Don't create empty /sys/devices/system/cpu/cpufreq directory"
    This reverts commit 4aed7025ee2b575cabecf249aae0c442d51061c0.
    
    We will change the global kobject to global kset for cpufreq driver.
    All the kobject in cpufreq driver, include global kobject and policy
    kobject, will use the global kset as its kset to fix uevent broken
    issue in cpufreq driver.
    
    Change-Id: Ia7e90ba509c7c872ea507a341657ccff07ed469d
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/755687
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Reviewed-by: Ravi Chebolu <arc095@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Fix files conflicts and dependencies
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- drivers/cpufreq/cpufreq_barry_allen.c
+++ drivers/cpufreq/cpufreq_barry_allen.c
@@ -1351,9 +1351,6 @@ static int cpufreq_governor_barry_allen(struct cpufreq_policy *policy,
 			return 0;
 		}
 
-		if (!have_governor_per_policy())
-			WARN_ON(cpufreq_get_global_kobject());
-
 		rc = sysfs_create_group(get_governor_parent_kobj(policy),
 				&barry_allen_attr_group);
 		if (rc) {
@@ -1389,8 +1386,6 @@ static int cpufreq_governor_barry_allen(struct cpufreq_policy *policy,
 		idle_notifier_unregister(&cpufreq_barry_allen_idle_nb);
 		sysfs_remove_group(get_governor_parent_kobj(policy),
 				&barry_allen_attr_group);
-		if (!have_governor_per_policy())
-			cpufreq_put_global_kobject();
 		mutex_unlock(&gov_lock);
 
 		break;
