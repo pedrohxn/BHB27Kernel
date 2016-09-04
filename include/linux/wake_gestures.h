f5d2e0b2afc88fbd622b7a00fb1710a583d39688
bhb27 <fglfgl27@gmail.com>
2016-09-04 16:37:58 -0300

    wake_gesture: Fix DT2W for quark Shamu has DT2W buid in, flar2 wake code only enable DT2W if his S2W is on
    Thanks @flar2 for point that out
    
    Also fix missing camera_switch in atmel_mxt_ts_mmi.c
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- include/linux/wake_gestures.h
+++ include/linux/wake_gestures.h
@@ -29,7 +29,6 @@ extern bool dt2w_switch;
 extern int vib_strength;
 bool scr_suspended(void);
 bool get_internal_dt(void);
-void set_internal_dt(bool input);
 void wg_setdev(struct input_dev *);
 void set_vibrate(int value);
 
