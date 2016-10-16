a75bdcee223264b7f72c774c9c8f644c8888d151
bhb27 <fglfgl27@gmail.com>
2016-10-16 21:05:47 -0200

    build: rr.sh add back iSu support turns out this make the device pass the safety verification
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- build/bhbkernel/ramdisk/custom/sbin/post-init-rr.sh
+++ build/bhbkernel/ramdisk/custom/sbin/post-init-rr.sh
@@ -15,6 +15,24 @@ if [ -e /system/app/Adaway/lib/arm/libtcpdump_exec.so ]; then
 
 fi
 
+# Isu support
+if [ -e /system/bin/temp_su ]; then
+	mv /system/bin/temp_su /system/bin/su
+fi
+
+if [ -e /system/xbin/isu ]; then
+	mv /system/xbin/isu /system/xbin/su
+	if [ ! -e /system/bin/su ]; then
+		ln -s -f /system/xbin/su /system/bin/su
+	fi
+# Isu end
+fi
+
+# give su root:root to adb su work optional/recommended
+if [ -e /system/xbin/su ]; then
+	chown root:root /system/xbin/su
+fi
+
 fsgid=`getprop ro.boot.fsg-id`;
 device=`getprop ro.boot.hardware.sku`
 
