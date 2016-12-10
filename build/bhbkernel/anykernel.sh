c2761e7e8d9a540690cfd9d5d8b30891c22e9886
Felipe Leon <fglfgl27@gmail.com>
2016-12-10 01:18:25 -0200

    build patch fstab
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build/bhbkernel/anykernel.sh
+++ build/bhbkernel/anykernel.sh
@@ -270,6 +270,7 @@ replace_string init.recovery.qcom.rc "interactive" "ondemand" "interactive"
 replace_string fstab.qcom "usb:auto" "usbdisk:auto" "usb:auto"
 replace_file init.qcom.power.rc 0750 init.qcom.power.rc
 replace_file init.qcom.rc 0750 init.qcom.rc
+replace_file fstab.qcom 0640 fstab.qcom
 remove_file busybox sbin
 
 # end ramdisk changes
