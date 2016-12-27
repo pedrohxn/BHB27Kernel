b988bf6b0014d5652304a9231839eb72162d77ba
Felipe Leon <fglfgl27@gmail.com>
2016-12-27 21:27:36 -0200

    proc: Remove duplicated androidboot flag from /proc/cmdline
    base on
    https://github.com/bhb27/BHB27Kernel/commit/994b2cb5bc16ff1524452b1aaef35ec2d69b85d6
    
    this flags are duplicated because of this
    https://github.com/bhb27/BHB27Kernel/commit/35f9117a988f162d6df15981b77f9874c4c55b00
    
    androidboot.flash.locked=0 is add by Moto bootloader and is used by safety net and produce a fail cts check in Moto devices
    
    this are the props after patch
    
    [ro.boot.baseband]: [mdm]
    [ro.boot.bl_state]: [0]
    [ro.boot.bootdevice]: [msm_sdcc.1]
    [ro.boot.bootloader]: [0x7094]
    [ro.boot.bootreason]: [reboot]
    [ro.boot.carrier]: [retbr]
    [ro.boot.cid]: [0xC]
    [ro.boot.device]: [quark]
    [ro.boot.flash.locked]: [1]
    [ro.boot.fsg-id]: [singlela]
    [ro.boot.hardware]: [qcom]
    [ro.boot.hardware.sku]: [XT1225]
    [ro.boot.hwrev]: [0x84A0]
    [ro.boot.mode]: [normal]
    [ro.boot.powerup_reason]: [0x00004000]
    [ro.boot.radio]: [0x5]
    [ro.boot.secure_hardware]: [1]
    [ro.boot.selinux]: [permissive]
    [ro.boot.verifiedbootstate]: [green]
    [ro.boot.write_protect]: [0]
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -31,22 +31,60 @@ static int __init proc_cmdline_init(void)
 	strcpy(cmd, saved_command_line);
 
 	/*
-	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
+	 * Remove 'androidboot.' "bad" or duplicated flags from command line seen
 	 * by userspace in order to pass SafetyNet CTS check.
 	 */
-	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
+	offset_addr = strstr(cmd, "androidboot.bl_state=2");
 	if (offset_addr) {
 		size_t i, len, offset;
 
 		len = strlen(cmd);
 		offset = offset_addr - cmd;
-
 		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ')
+			if (cmd[offset + i] == ' ') {
 				break;
+			}
 		}
 
-		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+		/*
+		 * if ((len - i - offset) == 0) argument is the last and have no space after
+		* when that happens add a space before the argument just to remove it
+		 */
+		if ((len - i - offset) == 0) {
+			offset_addr = strstr(cmd, " androidboot.bl_state=2");
+			len = strlen(cmd);
+			offset = offset_addr - cmd;
+			i = i + 1;
+			memmove(offset_addr, &cmd[offset + i + (i - 3)], i);
+		} else {
+			memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+		}
+	}
+
+	offset_addr = strstr(cmd, "androidboot.flash.locked=0");
+	if (offset_addr) {
+		size_t i, len, offset;
+
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ') {
+				break;
+			}
+		}
+		/*
+		 * if ((len - i - offset) == 0) argument is the last and have no space after
+		* when that happens add a space before the argument just to remove it
+		 */
+		if ((len - i - offset) == 0) {
+			offset_addr = strstr(cmd, " androidboot.flash.locked=0");
+			len = strlen(cmd);
+			offset = offset_addr - cmd;
+			i = i + 1;
+			memmove(offset_addr, &cmd[offset + i + (i - 3)], i);
+		} else {
+			memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+		}
 	}
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
