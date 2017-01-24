ff18ea12cce8fffd140c49af6cd110ca98690b5e
Felipe Leon <fglfgl27@gmail.com>
2017-01-24 14:15:52 -0200

    build use arm-eabi-4.9-ubertc
    warning reference
    https://github.com/bhb27/BHB27Kernel/commit/d44e43e6ff1b5b0de084fe577b36e3d775f678c1
    https://github.com/bhb27/BHB27Kernel/commit/3ce8d1062b7afae7853b1a582e95d6276aa97350
    https://github.com/bhb27/BHB27Kernel/commit/4416026391a7f5f94cf8883383a9a7f8b371ec43
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build/how_to_build_this.sh
+++ build/how_to_build_this.sh
@@ -41,7 +41,8 @@ cd $FOLDER;
 
 # CROSS_COMPILE toolchain folder
 #export CROSS_COMPILE=$HOME/android/n/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
-export CROSS_COMPILE=$HOME/android/temp/arm-eabi-4.9/bin/arm-eabi-
+#export CROSS_COMPILE=$HOME/android/temp/arm-eabi-4.9/bin/arm-eabi-
+export CROSS_COMPILE=$HOME/android/temp/arm-eabi-4.9-ubertc/bin/arm-eabi-
 #kernel zip name
 ZIPNAME="BHB27-Kernel-V37.zip";
 
