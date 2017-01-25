53fe070a293c17d9427b7d4b948340c923809927
Felipe Leon <fglfgl27@gmail.com>
2017-01-25 18:23:11 -0200

    V 3.8
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build/how_to_build_this.sh
+++ build/how_to_build_this.sh
@@ -44,7 +44,7 @@ cd $FOLDER;
 #export CROSS_COMPILE=$HOME/android/temp/arm-eabi-4.9/bin/arm-eabi-
 export CROSS_COMPILE=$HOME/android/temp/arm-eabi-4.9-ubertc/bin/arm-eabi-
 #kernel zip name
-ZIPNAME="BHB27-Kernel-V37.zip";
+ZIPNAME="BHB27-Kernel-V38.zip";
 
 #arch and out folder
 export ARCH=arm
