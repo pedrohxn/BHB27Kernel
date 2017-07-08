b9a6a7e9c55c71d4d6f8396a5ec507838247f7bd
Felipe Leon <fglfgl27@gmail.com>
2017-07-08 13:21:08 -0300

    Revert "cfg80211: Dynamic channel bandwidth changes in AP mode"
    This reverts commit 5893b978595b5733d4bc36464ec6e93799d24dc9.

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -1835,20 +1835,18 @@ static int nl80211_parse_chandef(struct cfg80211_registered_device *rdev,
 }
 
 static int __nl80211_set_channel(struct cfg80211_registered_device *rdev,
-				 struct net_device *dev,
+				 struct wireless_dev *wdev,
 				 struct genl_info *info)
 {
 	struct cfg80211_chan_def chandef;
 	int result;
 	enum nl80211_iftype iftype = NL80211_IFTYPE_MONITOR;
-	struct wireless_dev *wdev = NULL;
 
-	if (dev)
-		wdev = dev->ieee80211_ptr;
-	if (!nl80211_can_set_dev_channel(wdev))
-		return -EOPNOTSUPP;
 	if (wdev)
 		iftype = wdev->iftype;
+
+	if (!nl80211_can_set_dev_channel(wdev))
+		return -EOPNOTSUPP;
 
 	result = nl80211_parse_chandef(rdev, info, &chandef);
 	if (result)
@@ -1858,26 +1856,13 @@ static int __nl80211_set_channel(struct cfg80211_registered_device *rdev,
 	switch (iftype) {
 	case NL80211_IFTYPE_AP:
 	case NL80211_IFTYPE_P2P_GO:
+		if (wdev->beacon_interval) {
+			result = -EBUSY;
+			break;
+		}
 		if (!cfg80211_reg_can_beacon(&rdev->wiphy, &chandef)) {
 			result = -EINVAL;
 			break;
-		}
-		if (wdev->beacon_interval) {
-			if (!dev || !rdev->ops->set_ap_chanwidth ||
-			    !(rdev->wiphy.features &
-			      NL80211_FEATURE_AP_MODE_CHAN_WIDTH_CHANGE)) {
-				result = -EBUSY;
-				break;
-			}
-
-			/* Only allow dynamic channel width changes */
-			if (chandef.chan != wdev->preset_chandef.chan) {
-				result = -EBUSY;
-				break;
-			}
-			result = rdev_set_ap_chanwidth(rdev, dev, &chandef);
-			if (result)
-				break;
 		}
 		wdev->preset_chandef = chandef;
 		result = 0;
@@ -1901,7 +1886,7 @@ static int nl80211_set_channel(struct sk_buff *skb, struct genl_info *info)
 	struct cfg80211_registered_device *rdev = info->user_ptr[0];
 	struct net_device *netdev = info->user_ptr[1];
 
-	return __nl80211_set_channel(rdev, netdev, info);
+	return __nl80211_set_channel(rdev, netdev->ieee80211_ptr, info);
 }
 
 static int nl80211_set_wds_peer(struct sk_buff *skb, struct genl_info *info)
@@ -2035,10 +2020,9 @@ static int nl80211_set_wiphy(struct sk_buff *skb, struct genl_info *info)
 	}
 
 	if (info->attrs[NL80211_ATTR_WIPHY_FREQ]) {
-		result = __nl80211_set_channel(
-			rdev,
-			nl80211_can_set_dev_channel(wdev) ? netdev : NULL,
-			info);
+		result = __nl80211_set_channel(rdev,
+				nl80211_can_set_dev_channel(wdev) ? wdev : NULL,
+				info);
 		if (result)
 			goto bad_res;
 	}
